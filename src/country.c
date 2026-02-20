a53c0c577512e050708d984c46bca953103de550
Mav <riherr00@gmail.com>
2026-02-20 15:57:23 +0300

    Testing report

--- src/country.c
+++ src/country.c
@@ -61,7 +61,7 @@ int matches_criteria(const Country *country) {
 }
 
 void country_to_string(const Country *country, char *buffer) {
-    sprintf(buffer, "%-20s | %-20s | %-10s | %-16s | %6d тыс. | %7.1f тыс.км² | %-15s | %-35s | %-15s",
+    sprintf(buffer, "%-20s | %-20s | %-10s | %-16s | %6d тыс. | %7.1f тыс.км^2 | %-15s | %-35s | %-15s",
             country->name,
             country->capital,
             country->continent,
